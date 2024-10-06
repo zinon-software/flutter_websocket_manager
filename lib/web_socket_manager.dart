7f830b7a510d39ba3170da47c165f1faa9933efa
zinon-software <business.zinon@gmail.com>
2024-10-06 08:44:53 +0300

    "Removed entire GNU General Public License (GPL) text and replaced it with 'TODO: Add your license here.'"

---
+++ lib/web_socket_manager.dart
@@ -0,0 +1,47 @@
+library web_socket_manager;
+
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+class WebSocketManager {
+  final String url;
+  Map<String, dynamic>? queryParameters;
+  late WebSocketChannel _channel;
+  late SocketConnectionState _state;
+  Function(dynamic)? _callback;
+  Function(dynamic)? _errorCallback;
+
+  WebSocketManager(this.url, {this.queryParameters}) {
+    _state = SocketConnectionState.none; // Default state is none
+  }
+
+  void connect() async {
+    // Handle connection logic here
+  }
+
+  void sendMessage(Map<String, dynamic> message) {
+    _channel.sink.add(jsonEncode(message));
+  }
+
+  void disconnect() {
+    if (state == SocketConnectionState.connected) {
+      _channel.sink.close(3000); // Close connection
+      _state = SocketConnectionState.disconnected;
+    }
+  }
+
+  void setMessageCallback(Function(dynamic) callback) {
+    _callback = callback;
+  }
+
+  void setErrorCallback(Function(dynamic) callback) {
+    _errorCallback = callback;
+  }
+
+  SocketConnectionState get state => _state;
+}
+
+enum SocketConnectionState { connected, disconnected, none }
